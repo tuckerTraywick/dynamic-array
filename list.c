9f5f391a053a55217973e85c4e2db7a136c8e62d
Tucker Traywick <tucker.traywick@gmail.com>
2024-06-15 16:06:52 -0400

    Changed implementation.

--- list.c
+++ list.c
@@ -1,106 +1,19 @@
-#include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 #include "list.h"
 
-// Resizes a list to best fit its length.
-static void ListRealloc(struct List *list) {
-    if (list->length == list->capacity) {
-        list->capacity *= 2;
-    } else if (list->length < list->capacity/2) {
-        list->capacity /= 2;
-    }
-    void *newElements = realloc(list->elements, list->elementSize*list->capacity);
-    // TODO: Handle failed `realloc()`.
-    assert(newElements && "`realloc()` failed.");
-    list->elements = newElements;
+struct List {
+    size_t capacity;
+    size_t count;
+    size_t elementSize;
+    char elements[];
+};
+
+void *ListCreate(size_t capacity, size_t elementSize) {
+    struct List *list = malloc(sizeof *list + capacity*elementSize);
+    return &list->elements;
 }
 
-struct List ListCreate(size_t elementSize, size_t capacity) {
-    void *elements = malloc(elementSize*capacity);
-    // TODO: Handle failed `malloc()`.
-    assert(elements && "`malloc()` failed.");
-    return (struct List){
-        .elementSize = elementSize,
-        .capacity = capacity,
-        .length = 0,
-        .elements = elements,
-    };
-}
-
-void ListDestroy(struct List *list) {
-    assert(list);
-
-    free(list->elements);
-    *list = (struct List){0};
-}
-
-void *ListGet(struct List *list, size_t index) {
-    assert(list);
-    assert(index < list->length && "Index out of bounds.");
-
-    return (char*)list->elements + list->elementSize*index;
-}
-
-void ListSet(struct List *list, size_t index, void *element) {
-    assert(list);
-    assert(element);
-    assert(index < list->length && "Index out of bounds.");
-
-    memcpy((char*)list->elements + list->elementSize*index, element, list->elementSize);
-}
-
-void ListSwap(struct List *list, size_t indexA, size_t indexB) {
-    assert(list);
-    assert(indexA < list->length && "`indexA` out of bounds.");
-    assert(indexB < list->length && "`indexB` out of bounds.");
-
-    // Equivalent to: `temp = list[indexA]; list[indexA] = list[indexB]; list[indexB] = temp;`
-    void *temp = malloc(list->elementSize);
-    // TODO: Handle failed `malloc()`.
-    assert(temp && "`malloc()` failed.");
-    memcpy(temp, ListGet(list, indexA), list->elementSize);
-    memcpy(ListGet(list, indexA), ListGet(list, indexB), list->elementSize);
-    memcpy(ListGet(list, indexB), temp, list->elementSize);
-}
-
-void ListInsert(struct List *list, size_t index, void *element) {
-    assert(list);
-    assert(element);
-    assert(index <= list->length && "Index out of bounds.");
-    assert(list->length <= list->capacity && "Length incremented too much.");
-
-    ListRealloc(list);
-    // Shift the elements starting at `index` one to the right to make room for the new element.
-    memmove(
-        (char*)list->elements + list->elementSize*(index + 1),
-        (char*)list->elements + list->elementSize*index,
-        list->elementSize*(list->length - index)
-    );
-    memcpy((char*)list->elements + list->elementSize*index, element, list->elementSize);
-    ++list->length;
-}
-
-void ListAppend(struct List *list, void *element) {
-    assert(list);
-    assert(element);
-
-    ListRealloc(list);
-    memcpy((char*)list->elements + list->elementSize*list->length, element, list->elementSize);
-    ++list->length;
-}
-
-void ListRemove(struct List *list, size_t index) {
-    assert(list);
-    assert(index < list->length && "Index out of bounds.");
-
-    // Shift the elements starting at `index` one to the left.
-    memmove(
-        (char*)list->elements + list->elementSize*index,
-        (char*)list->elements + list->elementSize*(index + 1),
-        list->elementSize*(list->length - index)
-    );
-    --list->length;
-    ListRealloc(list);
+void ListDestroy(void *list) {
+    free((char*)list - sizeof (struct List));
 }
--- list.h
+++ list.h
@@ -3,38 +3,8 @@
 
 #include <stddef.h>
 
-// Represents a dynamic array that can grow and shrink at runtime.
-struct List {
-    size_t elementSize; // The size in bytes of one element.
-    size_t capacity; // The number of elements allocated.
-    size_t length; // The current number of elements.
-    void *elements; // The buffer containing the elements.
-};
+void *ListCreate(size_t capacity, size_t elementSize);
 
-// Returns a new list. The list must be destroy by `listDestroy()`.
-struct List ListCreate(size_t elementSize, size_t capacity);
-
-// Deallocates a list's elements and zeroes its memory. Be sure to destroy each element if needed
-// before calling this function.
-void ListDestroy(struct List *list);
-
-// Returns a pointer to the element at `index`.
-void *ListGet(struct List *list, size_t index);
-
-// Copies `element` to the element at `index`.
-void ListSet(struct List *list, size_t index, void *element);
-
-// Swaps the elements at `indexA` and `indexB`.
-void ListSwap(struct List *list, size_t indexA, size_t indexB);
-
-// Inserts `element` at `index`.
-void ListInsert(struct List *list, size_t index, void *element);
-
-// Appends `element` to the end of the list.
-void ListAppend(struct List *list, void *element);
-
-// Removes the element at `index`. Be sure to destroy the element if needed before calling this
-// function.
-void ListRemove(struct List *list, size_t index);
+void ListDestroy(void *list);
 
 #endif // LIST_H
